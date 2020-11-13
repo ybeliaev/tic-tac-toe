437c0e2825c0bcfd8b1d1098c5769e37e7e8bbcd
Jorgen <ybeliaev@gmail.com>
2020-11-13 21:01:17 +0200

    created board & square comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Board from './components/Board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Lean React</h1>
-      </header>
+    <div className="App">      
+        <h1>Lean React</h1>   
+        <Board/>   
     </div>
   );
 }
