84c84495490cff77e0d8753154d0202f0098703a
Jorgen <ybeliaev@gmail.com>
2020-11-14 17:00:24 +0200

    calc winner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React, {useState} from 'react';
+
 import './styles/root.scss';
 import Board from './components/Board';
+import { calculateWinner } from './helpers';
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXnext, setIsXnext] = useState(false);
+
+  const winner = calculateWinner(board)
+  console.log("Winner: ", winner)
+
+  const message = winner ? `Winner is ${winner}` : `Next player is ${isXnext?"X":"O"}`
+
+  const handleSquareClick = (position) => {
+    if (board[position] || winner) {
+      return;
+    }
+    setBoard((prev) => {
+      console.log("prev state: ", prev);
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXnext ? "X" : "O";
+        }
+        return square;
+      });
+    });
+    setIsXnext((prev) => !prev);
+  };
   return (
     <div className="app">      
-        <h1>Lean React</h1>   
-        <Board/>   
+        <h1>Lean React</h1> 
+        <h2>{message}</h2>  
+        <Board handleSquareClick={handleSquareClick} board={board}/>   
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,7 @@
 import React, { useState } from "react";
 import Square from "./Square";
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXnext, setIsXnext] = useState(false);
-
-  const handleSquareClick = (position) => {
-    if (board[position]) {
-      return;
-    }
-    setBoard((prev) => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXnext ? "X" : "O";
-        }
-        return square;
-      });
-    });
-    setIsXnext((prev) => !prev);
-  };
-
+const Board = ({ handleSquareClick, board }) => {
   const renderSquare = (position) => {
     return (
       <Square
