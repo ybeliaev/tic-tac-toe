70a2a9f00d38e13b0b278ab36a44b79cc26aa4ad
Jorgen <ybeliaev@gmail.com>
2020-11-16 12:37:26 +0200

    displayed game history

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 import './styles/root.scss';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 
 function App() {
@@ -32,11 +33,17 @@ function App() {
     });
     setCurrentMove(prev=>prev+1)
   };
+
+  const moveTo = (move) => {
+    setCurrentMove(move)
+  }
+
   return (
     <div className="app">      
         <h1>Lean React</h1> 
         <h2>{message}</h2>  
         <Board handleSquareClick={handleSquareClick} board={current.board}/>   
+        <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 }
